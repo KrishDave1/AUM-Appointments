35dba7643d537f6a28a4168a09da67fe38e73f9b
KrishDave1 <Krish.Dave@iiitb.ac.in>
2026-01-13 17:42:36 +0530

    fix errors

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,4 @@
-import { join } from "path";
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  outputFileTracingIncludes: {
-    "/api/**/*": [join(process.cwd(), "src", "prisma-client", "**", "*")],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
